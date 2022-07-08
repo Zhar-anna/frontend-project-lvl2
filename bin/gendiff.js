f165b67364d38cae6a379ea9505807afcf94bc42
Anna Zharova <a.zharova.muctr@gmail.com>
2022-07-08 20:38:08 +0300

    steps two arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,5 +3,8 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program
   .version('0.0.1', '-v, --version' , 'output the version number')
-  .description('Compares two configuration files and show a difference.');
+  .description('Compares two configuration files and show a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
 program.parse();
