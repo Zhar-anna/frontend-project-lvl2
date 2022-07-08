3ee76183b5884a0e4d8bb4bbe9a49e8017e2f1f2
Anna Zharova <a.zharova.muctr@gmail.com>
2022-07-08 13:35:09 +0300

    settings gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,7 @@
-import { Command } from 'commander';
+#!/usr/bin/env node
+import { Command } from 'commander/esm.mjs';
+const program = new Command();
+program
+  .version('0.0.1', '-v, --version' , 'output the version number')
+  .description('Compares two configuration files and show a difference.');
+program.parse();
