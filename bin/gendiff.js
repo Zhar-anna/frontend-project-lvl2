b95497d992e68bab2155a75788f00e056b7bd3bd
Anna Zharova <a.zharova.muctr@gmail.com>
2022-07-11 17:05:43 +0300

    steps 3 after video

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,11 +6,13 @@ const program = new Command();
 program
   .version('0.0.1', '-v, --version','output the version number')
   .description('Compares two configuration files and show a diference.')
-  .arguments('<filepath1> <filepath2>')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2, option) => {
+  .action((filepath1, filepath2) => {
     //const diference = option.f ? 1 : undefined;
-    console.log(str.split(option.separator, limit));//результат из индекса
+    console.log(genDiff(filepath1, filepath2));
+    //console.log(str.split(option.separator, limit));//результат из индекса
   });
   
-program.parse();
+program.parse(process.argv);
--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-import { fs } from 'node:fs';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import  { readFileSync }  from 'fs';
+//import path from 'path';
+import _ from lodash;
+//import { fileURLToPath } from 'url';
+//import { dirname } from 'path';
 
-const genDiff => {
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const genDiff = (filepath1, filepath2) => {
+
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = dirname(__filename);
+//const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+//const absoluteFile = path.resolve(filepath1 );
 
 //начала я использую path.resolve(process.cwd(), file1).trim(), затем читаю с помощью readFileSync и затем уже преобразую в объект с помощью JSON.parse()
 //функция должна принимать на вход пути до файлов, и возвращать результат сравнения
-const file1 = fs.readFileSync('<file1.json>'); //получить содержимое файловстроку в формате JSON
-const file1obj = JSON.parse(file1);
+const dataFromFilepath1 = readFileSync(filepath1, 'utf8'); //получить содержимое файлов - строку в формате JSON
+const dataFromFilepath2 = readFileSync(filepath2, 'utf8');
+
+const file1obj = JSON.parse(dataFromFilepath1);//содержимое файлов преобразовать в объект.
+const file2obj = JSON.parse(dataFromFilepath2);
+
+const arrKeyFromFile1 = Object.key(file1obj); //Получите все ключи из обоих объектов
+const arrKeyFromFile2 = Object.key(file2obj);
+
+const unionKeys = _.union(arrKeyFromFile1, arrKeyFromFile2); // затем найдите их объединение, тот самый единый массив. 
+
 };
 export default genDiff;
-//содержимое файлов преобразовать в объект. Именно с полученными объектами работает функция, сравнивающая их по ключам и значениям и собирающая строку-результат.
+ //Именно с полученными объектами работает функция, сравнивающая их по ключам и значениям и собирающая строку-результат.
 
-// Получите все ключи из обоих объектов, затем найдите их объединение, тот самый единый массив. Затем сравнивайте и проверяйте, что стало с каждым из ключей. (проходим reduce по уникальным ключам 4) в момент прохода reduce выполняется проверка на 4 условия (добавлено, удалено, измененно. не изменено)
+// , Затем сравнивайте и проверяйте, что стало с каждым из ключей. (проходим reduce по уникальным ключам 4) в момент прохода reduce выполняется проверка на 4 условия (добавлено, удалено, измененно. не изменено)
 //Если ключ есть в первом объекте, но отсутствует во втором - значит удалён. Если отсутствует в первом, но есть во втором - добавлен.  Когда вы встретили ключ, который изменён, сначала запишите в результат значение из первого объекта, а затем из второго.
 //редьюсом получил новый объект
 
