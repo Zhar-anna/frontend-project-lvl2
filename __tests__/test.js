4679cc90caba007785ac6672f603f3a58538fd7b
Anna Zharova <a.zharova.muctr@gmail.com>
2022-07-15 13:27:57 +0300

    steps 4 full

--- __tests__/test.js
+++ __tests__/test.js
@@ -9,8 +9,10 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const actual1 = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+const expected1 = readFile('result.txt');
+console.log('exp==', expected1);
+console.log('act==', actual1);
 test('result must be equal "result.txt"', () => {
-  const actual1 = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const expected1 = readFile('result.txt');
   expect(actual1).toEqual(expected1);
 });
