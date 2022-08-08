5dade918420917f2a0e3c0c85071e8b46e1bff4b
Anna Zharova <a.zharova.muctr@gmail.com>
2022-08-08 15:02:23 +0300

    two step rewiev

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const indent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(depth * spacesCount - 2);
+const makeIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(depth * spacesCount - 2);
 
 const stringify = (data, depth = 1) => {
   if (!_.isObject(data)) {
@@ -10,26 +10,16 @@ const stringify = (data, depth = 1) => {
   const result = keys.map((key) => {
     const prop = data[key];
 
-    return `${indent(depth + 1)}  ${key}: ${stringify(prop, depth + 1)}`;
+    return `${makeIndent(depth + 1)}  ${key}: ${stringify(prop, depth + 1)}`;
   });
 
-  return `{\n${result.join('\n')}\n  ${indent(depth)}}`;
+  return `{\n${result.join('\n')}\n  ${makeIndent(depth)}}`;
 };
-//   const indentForKey = replacer.repeat(depth + 3);
-//   const indentForBracket = replacer.repeat(depth + 1);
-//   const lines = Object.entries(data)
-//     .map(([key, value]) => `${indentForKey}${key}: ${stringify(value, depth + 2, replacer)}`);
 
-//   return ['{', ...lines, `${indentForBracket}}`].join('\n');
-// };
 const stylish = (diff, depth = 0) => {
   const {
     name, value, type, value1, value2, children,
   } = diff;
-  // const indent = replacer.repeat(depth);
-  // const indentForBracket = replacer.repeat(depth + 1);
-
-  // const makeLine = (val, mark) => `${indent}${mark} ${name}: ${stringify(val, depth, replacer)}`;
 
   switch (type) {
     case 'root': {
@@ -38,51 +28,22 @@ const stylish = (diff, depth = 0) => {
     }
     case 'nested': {
       const result = children.flatMap((child) => stylish(child, depth + 1));
-      return `${indent(depth)}  ${name}: {\n${result.join('\n')}\n${indent(depth)}  }`;
+      return `${makeIndent(depth)}  ${name}: {\n${result.join('\n')}\n${makeIndent(depth)}  }`;
     }
     case 'added':
-      return `${indent(depth)}+ ${name}: ${stringify(value, depth)}`;
+      return `${makeIndent(depth)}+ ${name}: ${stringify(value, depth)}`;
     case 'deleted':
-      return `${indent(depth)}- ${name}: ${stringify(value, depth)}`;
+      return `${makeIndent(depth)}- ${name}: ${stringify(value, depth)}`;
     case 'unchanged':
-      return `${indent(depth)}  ${name}: ${stringify(value, depth)}`;
+      return `${makeIndent(depth)}  ${name}: ${stringify(value, depth)}`;
     case 'changed': {
-      const removed = `${indent(depth)}- ${name}: ${stringify(value1, depth)}`;
-      const added = `${indent(depth)}+ ${name}: ${stringify(value2, depth)}`;
+      const removed = `${makeIndent(depth)}- ${name}: ${stringify(value1, depth)}`;
+      const added = `${makeIndent(depth)}+ ${name}: ${stringify(value2, depth)}`;
       return `${removed}\n${added}`;
     }
     default:
       throw new Error(`Type: ${type} is undefined`);
   }
 };
-// const stylish = (diff, replacer = '  ') => {
-//   const iter = (tree, depth) => tree.map((node) => {
-//     const { name, value, type } = node;
-//     const indent = replacer.repeat(depth);
-//     const indentForBracket = replacer.repeat(depth + 1);
-
-//     const makeLine = (val, mark) => `${indent}${mark} ${name}:
-// ${stringify(val, depth, replacer)}`;
-
-//     switch (type) {
-//       case 'added':
-//         return makeLine(value, '+');
-//       case 'deleted':
-//         return makeLine(value, '-');
-//       case 'nested':
-//         return `${indent}  ${name}: ${['{', ...iter(value,
-// depth + 2), `${indentForBracket}}`].join('\n')}`;
-//       case 'unchanged':
-//         return makeLine(value, ' ');
-//       case 'changed':
-//         return [`${makeLine(node.value1, '-')}`,
-//           `${makeLine(node.value2, '+')}`].join('\n');
-//       default:
-//         throw new Error(`Type: ${type} is undefined`);
-//     }
-//   });
-//   const stylishDiff = iter(diff, 1);
-//   return ['{', ...stylishDiff, '}'].join('\n');
-// };
 
 export default stylish;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import getParser from './parsers.js';
 import getFormat from './formatters/index.js';
 
 const getFileFormat = (filepath) => path.extname(filepath).slice(1);
+const getPathFile = (filepath) => path.resolve(process.cwd(), filepath).trim();
+const readFile = (filepath) => fs.readFileSync(getPathFile(filepath), 'utf-8');
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const getPathFile = (filepath) => path.resolve(process.cwd(), filepath).trim();
-  const readFile = (filepath) => fs.readFileSync(getPathFile(filepath), 'utf-8');
   const dataFromFilepath1 = readFile(filepath1);
   const dataFromFilepath2 = readFile(filepath2);
   const file1obj = getParser(dataFromFilepath1, getFileFormat(filepath1));
