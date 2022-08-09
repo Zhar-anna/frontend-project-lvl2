905dca846f92a8ebbefcf797256a41c291006746
Anna Zharova <a.zharova.muctr@gmail.com>
2022-08-09 18:58:49 +0300

    two step rewiev, naming

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,17 +2,15 @@ import _ from 'lodash';
 
 const makeIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(depth * spacesCount - 2);
 
-const stringify = (data, depth = 1) => {
+const valueFromation = (data, stylish, depth = 1) => {
   if (!_.isObject(data)) {
     return data;
   }
   const keys = Object.keys(data);
-  const result = keys.map((key) => {
-    const prop = data[key];
-
-    return `${makeIndent(depth + 1)}  ${key}: ${stringify(prop, depth + 1)}`;
+  const result = keys.map((name) => {
+    const value = data[name];
+    return stylish({ name, value, type: 'unchanged' }, depth + 1);
   });
-
   return `{\n${result.join('\n')}\n  ${makeIndent(depth)}}`;
 };
 
@@ -23,22 +21,22 @@ const stylish = (diff, depth = 0) => {
 
   switch (type) {
     case 'root': {
-      const result = children.flatMap((child) => stylish(child, depth + 1));
-      return `{\n${result.join('\n')}\n}`;
+      const resultLine = children.flatMap((child) => stylish(child, depth + 1));
+      return `{\n${resultLine.join('\n')}\n}`;
     }
     case 'nested': {
-      const result = children.flatMap((child) => stylish(child, depth + 1));
-      return `${makeIndent(depth)}  ${name}: {\n${result.join('\n')}\n${makeIndent(depth)}  }`;
+      const resultLine = children.flatMap((child) => stylish(child, depth + 1));
+      return `${makeIndent(depth)}  ${name}: {\n${resultLine.join('\n')}\n${makeIndent(depth)}  }`;
     }
     case 'added':
-      return `${makeIndent(depth)}+ ${name}: ${stringify(value, depth)}`;
+      return `${makeIndent(depth)}+ ${name}: ${valueFromation(value, stylish, depth)}`;
     case 'deleted':
-      return `${makeIndent(depth)}- ${name}: ${stringify(value, depth)}`;
+      return `${makeIndent(depth)}- ${name}: ${valueFromation(value, stylish, depth)}`;
     case 'unchanged':
-      return `${makeIndent(depth)}  ${name}: ${stringify(value, depth)}`;
+      return `${makeIndent(depth)}  ${name}: ${valueFromation(value, stylish, depth)}`;
     case 'changed': {
-      const removed = `${makeIndent(depth)}- ${name}: ${stringify(value1, depth)}`;
-      const added = `${makeIndent(depth)}+ ${name}: ${stringify(value2, depth)}`;
+      const removed = `${makeIndent(depth)}- ${name}: ${valueFromation(value1, stylish, depth)}`;
+      const added = `${makeIndent(depth)}+ ${name}: ${valueFromation(value2, stylish, depth)}`;
       return `${removed}\n${added}`;
     }
     default:
